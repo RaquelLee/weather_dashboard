7776a4b4352405b70e54e3a5110c183ca23a1812
rsco <RaquelLee@users.noreply.github.com>
2021-01-30 22:29:18 -0500

    user searches for city, local storage set and actionable list item created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
 cityName = localStorage.getItem("City");
 populatePage(cityName);
+
+$("#city-search").on("click", function(event) {
+    event.preventDefault();
+    var cityName = $("#city-input").val();
+    localStorage.setItem("City", cityName);
+    populatePage(cityName);
+    var li = $("<li>");
+    li.addClass("list-group-item list-group-item-action");
+    li.text(cityName);
+    $(li).on("click", function() {
+        populatePage(cityName);
+    });
+    $(".list-group").prepend(li);
+});
