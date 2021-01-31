2f9a1086073bbcc28e19839e2d72bc30eeca69e1
rsco <RaquelLee@users.noreply.github.com>
2021-01-30 22:31:58 -0500

    OW geo API used to grab city lattitude and longitude for use in one call API

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,19 @@ $("#city-search").on("click", function(event) {
     });
     $(".list-group").prepend(li);
 });
+
+function populatePage (cn){
+    var apiKey = "&appid=166935e80f8de25e6d29e85714fb2820";
+    var queryURL =
+    "http://api.openweathermap.org/geo/1.0/direct?q="
+    + cn + apiKey;
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(c) {
+    var cityNameObj = c[0].name;
+    var lat = c[0].lat;
+    var lon = c[0].lon;
+    });
+};
